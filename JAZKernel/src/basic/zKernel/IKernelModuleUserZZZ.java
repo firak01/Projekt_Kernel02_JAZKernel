b6e5e275b11bc564a0bfc1c7754084aac8ba3388
Firak01 <firak01@t-online.de>
2018-04-02 13:04:23 +0200

    Verbesserung der Kernelkonfigurationsnutzung
    Signed-off-by: Firak01 <firak01@t-online.de>

--- JAZKernel/src/basic/zKernel/IKernelModuleUserZZZ.java
+++ JAZKernel/src/basic/zKernel/IKernelModuleUserZZZ.java
@@ -6,15 +6,15 @@ package basic.zKernel;
 import basic.zBasic.ExceptionZZZ;
 
 
-/**Erweitere den Kernel um Methoden, mit denen aus der .ini - Datei der Kernelkonfiguration Parameter ausgelesen werden können.
- * Das das Kernelobjekt die Voraussetzung ist, aber nicht jede Klasse die Modulkonfiguration nutzen soll, ist dies eine Erweiterung.
- * 
+/**Erweitere den Kernel um Methoden, mit denen aus der .ini - Datei der Kernelkonfiguration Parameter ausgelesen werden kï¿½nnen.
+ * Da das Kernelobjekt die Voraussetzung ist, aber nicht jede Klasse die Modulkonfiguration nutzen soll, ist dies eine Erweiterung.
+ * 20180402: Manche Klassen sollen zwar Ihren Modulnamen, etc. angeben kÃ¶nnen, brauchen aber nicht das Kernel Objekt... darum nicht mehr: extends IKernelUserZZZ {
  * @author lindhaueradmin
  *
  * lindhaueradmin, 06.07.2013
  */
-public interface IKernelModuleUserZZZ extends IKernelUserZZZ {
-	public abstract String getModuleName();    //Z.B. der Name des übergeordneten JFrames. Ist der Package und Klassenname
+public interface IKernelModuleUserZZZ{ //FGL 20180402: Manche Klassen sollen zwar Ihren Modulnamen, etc. angeben kÃ¶nnen, brauchen aber nicht das Kernel Objekt... darum nicht mehr: extends IKernelUserZZZ {
+	public abstract String getModuleName();    //Z.B. der Name des ï¿½bergeordneten JFrames. Ist der Package und Klassenname
 	public abstract String getProgramName() throws ExceptionZZZ;   //Z.B. ein Button, Ist der Package und Klassenname		
 	public abstract String getProgramAlias() throws ExceptionZZZ;  //Der Alias wird auf Modulebenen definiert. Package und Klassenname = Alias. Speicher den Alias in einer Variablen. 
 }
