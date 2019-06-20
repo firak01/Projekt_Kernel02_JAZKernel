23d051b4442abe1ad5dd182f20002c6748702ed5
firak01 <firak01@t-online.de>
2019-06-20 21:40:56 +0200

    1
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernel/src/basic/zBasic/util/datatype/counter/CounterHandlerSingleton_AlphanumericSignificantZZZ.java
+++ JAZKernel/src/basic/zBasic/util/datatype/counter/CounterHandlerSingleton_AlphanumericSignificantZZZ.java
@@ -11,6 +11,10 @@ public class CounterHandlerSingleton_AlphanumericSignificantZZZ {
 	private static CounterHandlerSingleton_AlphanumericSignificantZZZ objCounterSingleton = null; //muss static sein, wg. getInstance()!!!
 	
 	private HashMap<String,ICounterAlphanumericSignificantZZZ>hmCounter=new HashMap<String, ICounterAlphanumericSignificantZZZ>();
+	int iStart;;
+	ICounterStrategyAlphanumericSignificantZZZ objCounterStrategy;
+	int iCounterLengthDefault = 4;
+	char cCounterFillingDefault = 0;
 	
 	/**Konstruktor ist private, wg. Singleton
 	 * @param objKernel
@@ -47,12 +51,29 @@ public class CounterHandlerSingleton_AlphanumericSignificantZZZ {
 			String[] saCalling = ReflectCodeZZZ.getCallingStack();
 			
 			//Suche in der Hashmap nach einem String des Klassen.Methodennamens, der schon in der HashMap gespeichert ist.
-			//Wird er gefunden, dann soll der Counter wiederverwendet werden.
-			
-			
+			//Wird er gefunden, dann soll der Counter wiederverwendet werden.			
+			HashMap<String,ICounterAlphanumericSignificantZZZ> hmCounter = this.getCounterHashMap();
+			for(String sCalling : saCalling){
+				objCounter = hmCounter.get(sCalling);
+				if(objCounter!=null)break ;
+			}
+									
 			//Wenn es solch einen String nicht gibt, erzeuge neuen Counter und speichere in Hashmap unter dem aktuellen Methodennamen.
+			if(objCounter==null){
+				int iStart = this.getValueStart();
+				ICounterStrategyAlphanumericSignificantZZZ objCounterStrategy = this.getCounterStrategy();
+				CounterByCharacterAsciiFactoryZZZ objCounterFactory = CounterByCharacterAsciiFactoryZZZ.getInstance();
+				objCounter = objCounterFactory.createCounter(objCounterStrategy, iStart);
+				
+				String sCalling = ReflectCodeZZZ.getMethodCallingName();
+				this.setCounterFor(sCalling, objCounter);
+			}
 		}
 		return objCounter;
+	}
+	
+	private void setCounterFor(String sCalling, ICounterAlphanumericSignificantZZZ objCounter){
+		this.getCounterHashMap().put(sCalling, objCounter);
 	}
 	
 	private ICounterAlphanumericSignificantZZZ getCounterFor(String sKey){
@@ -61,5 +82,46 @@ public class CounterHandlerSingleton_AlphanumericSignificantZZZ {
 			
 		}
 		return objCounter;
+	}
+	
+	private HashMap<String,ICounterAlphanumericSignificantZZZ> getCounterHashMap(){
+		return this.hmCounter;
+	}
+	
+	public int getValueStart(){
+		return this.iStart;
+	}
+	public void setValueStart(int iStart){
+		this.iStart = iStart;
+	}
+	public ICounterStrategyAlphanumericSignificantZZZ getCounterStrategy(){
+		ICounterStrategyAlphanumericSignificantZZZ objReturn = null;
+		main:{
+			objReturn = this.objCounterStrategy;
+			if(objReturn==null){
+				objReturn = new CounterStrategyAlphanumericSignificantZZZ();
+				int iLength = this.getCounterLengthDefault();
+				objReturn.setCounterLength(iLength);
+				char cFilling = this.getCounterFillingDefault();
+				objReturn.setCounterFilling(cFilling);
+			}
+		}//end main:
+		return objReturn;
+	}
+	public void setCounterStrategy(ICounterStrategyAlphanumericSignificantZZZ objCounterStrategy){
+		this.objCounterStrategy = objCounterStrategy;
+	}
+	
+	public void setCounterLengthDefault(int iCounterLengthDefault){
+		this.iCounterLengthDefault = iCounterLengthDefault;		
+	}
+	public int getCounterLengthDefault(){
+		return this.iCounterLengthDefault;
+	}
+	public void setCounterFillingDefault(char cCounterFillingDefault){
+		this.cCounterFillingDefault = cCounterFillingDefault;
+	}
+	public char getCounterFillingDefault(){
+		return this.cCounterFillingDefault;
 	}
 }
