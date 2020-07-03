ceabf2652fad4a876409ef7af45a825512a560b3
firak01 <firak01@t-online.de>
2020-07-03 09:59:52 +0200

    TODO GOON: Irgendwann diese Konstante nur für Tests bereitstellen. Als extra unveränderliche JAR.
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernel/test/basic/zBasic/util/file/jar/ITestJarInfoConstants.java
+++ JAZKernel/test/basic/zBasic/util/file/jar/ITestJarInfoConstants.java
@@ -1,6 +1,6 @@
 package basic.zBasic.util.file.jar;
 
 public interface ITestJarInfoConstants {
-	public static final String kZIP_FILE_ONE = "c:\\1fgl\\client\\OVPN\\OpenVPNZZZ_V20200618.jar";
-	public static final String kJAR_FILE_ONE = "c:\\1fgl\\client\\OVPN\\OpenVPNZZZ_V20200618.jar";
+	public static final String kZIP_FILE_ONE = "c:\\1fgl\\client\\OVPN\\OpenVPNZZZ_V20200703.jar";
+	public static final String kJAR_FILE_ONE = "c:\\1fgl\\client\\OVPN\\OpenVPNZZZ_V20200703.jar";
 }
