971a63584d84e35d0a2b9e6ae2a48204e1648d8d
firak01 <firak01@t-online.de>
2020-12-19 12:20:18 +0100

    KernelAllTests für den Start als Jar vorbereiten. (Start klappt in der JRE7 Maschine, aber es werden viele Fehler geworfen,  wohl weil bestimmte Dateien nicht gefunden werden können).
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernel/test/basic/zBasic/util/file/jar/ITestJarInfoConstants.java
+++ JAZKernel/test/basic/zBasic/util/file/jar/ITestJarInfoConstants.java
@@ -1,6 +1,6 @@
 package basic.zBasic.util.file.jar;
 
 public interface ITestJarInfoConstants {
-	public static final String kZIP_FILE_ONE = "c:\\1fgl\\client\\OVPN\\OpenVPNZZZ_V20200703.jar";
-	public static final String kJAR_FILE_ONE = "c:\\1fgl\\client\\OVPN\\OpenVPNZZZ_V20200703.jar";
+	public static final String kZIP_FILE_ONE = "c:\\1fgl\\client\\OVPN\\OpenVPNZZZ.jar";
+	public static final String kJAR_FILE_ONE = "c:\\1fgl\\client\\OVPN\\OpenVPNZZZ.jar";
 }
