616e6c8d3132f924663859c83180c32bc921e419
firak01 <firak01@t-online.de>
2021-12-14 08:39:24 +0100

    Arbeit am Modul, auslesen aus dem UI
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernel/src/basic/zKernel/component/IKernelModuleZZZ.java
+++ JAZKernel/src/basic/zKernel/component/IKernelModuleZZZ.java
@@ -7,6 +7,8 @@ public interface IKernelModuleZZZ extends IFlagUserZZZ {
 	public enum FLAGZ{
 		ISKERNELMODULE;
 	}	
+	public static String sMODULE_UNDEFINED="Modul_undefined";
+	
 	public abstract String getModuleName() throws ExceptionZZZ;    //Z.B. der Name des uebergeordneten JFrames. Ist der Package und Klassenname
 	public abstract void resetModuleUsed();                      //wird das ISKERNELMODULE Flag neu auf true gesetzt, dann ist angeraten das durchzuführen. Somit würde ein ggfs. ererbtes anderes Modul entfernt und der Modulname neu geholt.
 }
