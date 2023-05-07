ddbea45a93856ff30234c533a1c6aeb7f1109f45
firak01 <firak01@t-online.de>
2023-05-07 10:55:18 +0200

    1
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ JAZKernel/src/basic/zKernel/IKernelConfigSectionEntryUserZZZ.java
@@ -0,0 +1,6 @@
+package basic.zKernel;
+
+public interface IKernelConfigSectionEntryUserZZZ {
+	public IKernelConfigSectionEntryZZZ getEntry();
+	public void setEntry(IKernelConfigSectionEntryZZZ objEntry);
+}
