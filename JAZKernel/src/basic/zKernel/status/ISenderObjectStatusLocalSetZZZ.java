46b60b8316ad33ada13e8510ac2d73eb646633f5
firak01 <firak01@t-online.de>
2023-08-03 09:55:07 +0200

    StatusLocal - Sender und Empfänger
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernel/src/basic/zKernel/status/ISenderObjectStatusLocalSetZZZ.java
+++ JAZKernel/src/basic/zKernel/status/ISenderObjectStatusLocalSetZZZ.java
@@ -2,6 +2,8 @@ package basic.zKernel.status;
 
 import java.util.ArrayList;
 
+import basic.zBasic.ExceptionZZZ;
+
 
 /**Dieses Interface enthaelt Methoden, die von den Klassen implementiert werden muessen, 
  * die den Kernel eigenen Event verwalten sollen.
@@ -12,9 +14,9 @@ import java.util.ArrayList;
 public interface ISenderObjectStatusLocalSetZZZ {
 	public abstract void fireEvent(IEventObjectStatusLocalSetZZZ event);
 
-	public abstract void removeListenerObjectStatusLocalSet(IListenerObjectStatusLocalSetZZZ objEventListener);
+	public abstract void removeListenerObjectStatusLocalSet(IListenerObjectStatusLocalSetZZZ objEventListener) throws ExceptionZZZ;
 
-	public abstract void addListenerObjectStatusLocalSet(IListenerObjectStatusLocalSetZZZ objEventListener);
+	public abstract void addListenerObjectStatusLocalSet(IListenerObjectStatusLocalSetZZZ objEventListener) throws ExceptionZZZ;
 	
-	public abstract ArrayList<IListenerObjectStatusLocalSetZZZ> getListenerRegisteredAll();
+	public abstract ArrayList<IListenerObjectStatusLocalSetZZZ> getListenerRegisteredAll() throws ExceptionZZZ;
 }
