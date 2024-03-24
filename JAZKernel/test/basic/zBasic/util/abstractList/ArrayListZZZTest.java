52b475bc3ae26a3bd2450b1932babc088e24ef53
firak01 <firak01@t-online.de>
2024-03-24 15:37:33 +0100

    1
    Signed-off-by: firak01 <firak01@t-online.de>

---
+++ JAZKernel/test/basic/zBasic/util/abstractList/ArrayListZZZTest.java
@@ -0,0 +1,50 @@
+package basic.zBasic.util.abstractList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import basic.zBasic.ExceptionZZZ;
+import basic.zBasic.util.abstractEnum.EnumSetMappedTestTypeZZZ;
+import basic.zBasic.util.abstractList.ArrayListExtendedZZZ;
+import basic.zBasic.util.abstractList.HashMapExtendedIndexedZZZ;
+
+import junit.framework.TestCase;
+
+public class ArrayListZZZTest extends TestCase{
+	private ArrayList alTest = null; 
+	 
+    protected void setUp(){
+      
+	//try {			
+		//### Die TestObjekte	
+    	alTest = new ArrayList();	
+    	
+    	
+	/*
+	} catch (ExceptionZZZ ez) {
+		fail("Method throws an exception." + ez.getMessageLast());
+	} 
+	*/
+	
+}//END setup
+    
+   public void testToArray() {
+	   //try {
+	   Class<?> objClass = EnumSetMappedTestTypeZZZ.class;
+	   Object[] objaEnum = objClass.getEnumConstants();
+	   
+	   //Erst einmal aus dem Enum eine ArrayList machen.
+	   ArrayListExtendedZZZ listaEnumExtended = new ArrayListExtendedZZZ(objaEnum);
+	   ArrayList listaEnum = listaEnumExtended.toArrayList();
+	   
+	   //Test: Aus der ArrayList ein Enum machen
+	   Object[] objaResult = ArrayListZZZ.toArray(listaEnum);
+	   assertNotNull(objaResult);
+	   
+	   //EnumSetMappedTestTypeZZZ
+	   System.out.println("test");
+//   		} catch (ExceptionZZZ ez) {
+//			fail("Method throws an exception." + ez.getMessageLast());
+//		}    
+   }
+}
