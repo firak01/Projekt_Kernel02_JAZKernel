672913975f8ea9b3c5c298c9b99de783d5663090
firak01 <firak01@t-online.de>
2024-06-18 15:12:17 +0200

    1b
    Signed-off-by: firak01 <firak01@t-online.de>

--- JAZKernel/test/basic/zBasic/util/log/LogStringZZZTest.java
+++ JAZKernel/test/basic/zBasic/util/log/LogStringZZZTest.java
@@ -1,23 +1,12 @@
 package basic.zBasic.util.log;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Vector;
-
-import junit.framework.TestCase;
-import basic.javagently.Stream;
 import basic.zBasic.DummyTestObjectZZZ;
 import basic.zBasic.ExceptionZZZ;
 import basic.zBasic.ReflectCodeZZZ;
 import basic.zBasic.util.datatype.string.StringZZZ;
-import basic.zBasic.util.file.FileEasyZZZ;
-import basic.zBasic.util.file.ini.IniFile;
-import basic.zBasic.util.file.txt.TxtWriterZZZ;
 import basic.zBasic.util.math.PrimeFactorizationZZZ;
 import basic.zBasic.util.math.PrimeNumberZZZ;
-import basic.zBasic.util.stream.IStreamZZZ;
-import basic.zBasic.util.stream.StreamZZZ;
+import junit.framework.TestCase;
 
 public class LogStringZZZTest  extends TestCase{
 	
