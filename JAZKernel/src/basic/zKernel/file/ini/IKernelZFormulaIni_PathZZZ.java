dadebea02c3e3388391ad32583bd34757e27943c
fl86kyvo <fl86kyvo@URZ-VWIT0-M011.adds.tu-freiberg.de>
2024-08-13 16:35:30 +0200

    Flag - Handling in vielen Klassen vervollständigt.

---
+++ JAZKernel/src/basic/zKernel/file/ini/IKernelZFormulaIni_PathZZZ.java
@@ -0,0 +1,25 @@
+package basic.zKernel.file.ini;
+
+import basic.zBasic.ExceptionZZZ;
+import basic.zBasic.IConvertableZZZ;
+import basic.zBasic.component.IModuleZZZ;
+import basic.zBasic.util.xml.tagsimple.IParseEnabledZZZ;
+
+public interface IKernelZFormulaIni_PathZZZ {
+	//Merke: Erst ab Java 8 können static Ausdrücke in ein interface: 
+	//getExpressionTagName()
+	//getExpressionTagStarting()
+	//getExpressionTagClosing()
+	//getExpressionTagEmpty()
+ 
+	public enum FLAGZ{
+		USEEXPRESSION_PATH
+	}	
+	
+	//damit muss man nicht mehr tippen hinter dem enum .name()
+	public boolean getFlag(IKernelZFormulaIni_PathZZZ.FLAGZ objEnumFlag);
+	public boolean setFlag(IKernelZFormulaIni_PathZZZ.FLAGZ objEnumFlag, boolean bFlagValue) throws ExceptionZZZ;
+	public boolean[] setFlag(IKernelZFormulaIni_PathZZZ.FLAGZ[] objEnumFlag, boolean bFlagValue) throws ExceptionZZZ;
+	public abstract boolean proofFlagExists(IKernelZFormulaIni_PathZZZ.FLAGZ objEnumFlag) throws ExceptionZZZ;
+	public abstract boolean proofFlagSetBefore(IKernelZFormulaIni_PathZZZ.FLAGZ objEnumFlag) throws ExceptionZZZ;
+}
